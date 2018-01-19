6cc5c89113114c4f413e2ff21c3d537b102cb6c3
tin <quangtin12b5@gmail.com>
2018-01-19 16:34:54 +0700

    changed i18n date to date time

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -4,10 +4,10 @@ class OrderSerializer < ActiveModel::Serializer
   has_many :product_orders, serializer: ProductOrderSerializer
 
   def time_in
-    I18n.l object.created_at, format: :full_date
+    I18n.l object.created_at, format: :full_datetime
   end
 
   def time_out
-    I18n.l object.updated_at, format: :full_date
+    I18n.l object.updated_at, format: :full_datetime
   end
 end
